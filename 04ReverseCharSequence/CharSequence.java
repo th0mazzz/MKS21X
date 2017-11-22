32b044d18de23bde45c7af2b080259ec83517d64
Thomas <tzhao2@stuy.edu>
2017-11-22 15:41:05 -0500

    Wrote ReverseCharSequence

---
+++ 04ReverseCharSequence/CharSequence.java
@@ -0,0 +1,6 @@
+public interface CharSequence{
+    public char charAt(int index);
+    public int length();
+    public CharSequence subSequence(int start, int end);
+    public String toString();
+}
