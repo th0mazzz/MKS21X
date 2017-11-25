5856dc462e0a95c204605fda92b29cd7de346119
Thomas <tzhao2@stuy.edu>
2017-11-25 00:17:43 -0500

    Updated and finished CharSequence interface and ReverseCharSequence class

--- 04ReverseCharSequence/CharSequence.java
+++ 04ReverseCharSequence/CharSequence.java
@@ -1,6 +1,6 @@
 public interface CharSequence{
     public char charAt(int index);
     public int length();
-    public CharSequence subSequence(int start, int end);
+    public ReverseCharSequence subSequence(int start, int end);
     public String toString();
 }
--- 04ReverseCharSequence/ReverseCharSequence.java
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -1,3 +1,43 @@
 public class ReverseCharSequence{
 
+    private String storage;
+    private String original;
+    
+    public ReverseCharSequence(String inputString){
+	storage = "";
+	original = inputString;
+	for(int index = inputString.length() - 1; index >= 0; index--){
+	    storage = storage + inputString.charAt(index);
+	}
+    }
+
+    public char charAt(int index){
+	return storage.charAt(index);
+    }
+    
+    public int length(){
+	return storage.length();
+    }
+
+    public ReverseCharSequence subSequence(int start, int end){
+	ReverseCharSequence theSub = new ReverseCharSequence(storage);
+	theSub.storage = storage.substring(start, end);
+	return theSub;
+    }
+
+    public String toString(){
+	return storage;
+    }
+    
+    public static void main(String[] args){
+	// Testing constructor
+	ReverseCharSequence test = new ReverseCharSequence("gnirts a si sihT");
+	System.out.println(test.storage); //prints This is a string
+	System.out.println(test.length()); //prints 16
+	System.out.println(test.charAt(2)); //prints i
+	System.out.println(test.subSequence(0, 4)); //prints This
+	System.out.println(test); //prints This is a string
+	//This last one tests the toString method of ReverseCharSequence
+	
+    }
 }
