7dc30ed0386a8b57f6953cc6c8e24ca246118706
Thomas <tzhao2@stuy.edu>
2017-11-28 14:34:16 -0500

    Wrote constructors, instance variables, and get methods

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,3 +1,19 @@
 public class Book{
+    private String author;
+    private String title;
+    private String ISBN;
+
+    public Book(){
+    }
+    
+    public Book(String name, String bookName, String number){
+	author = name;
+	title = bookName;
+	ISBN = number;
+    }
+
+    public String getAuthor(){return author}
+    public String getTitle(){return title}
+    public String getISBN(){return ISBN}
 
 }
