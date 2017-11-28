f29ccab0e96abb749001d88c921549dbd5831822
Thomas <tzhao2@stuy.edu>
2017-11-28 16:53:36 -0500

    Added all methods for ReferenceBook

--- 05Library/ReferenceBook.java
+++ 05Library/ReferenceBook.java
@@ -6,4 +6,18 @@ public class ReferenceBook extends LibraryBook{
 	super(name, bookName, number, callNum);
 	collection = collectName;
     }
+
+    public String getCollection(){return collection;}
+
+    public void checkout(String patron, String due){
+	System.out.println("Sorry, reference books are not avaliable for checkout");
+    }
+
+    public void returned(){
+	System.out.println("Impossible, shouldn't have been checked out in the first place");
+    }
+
+    public String circulationStatus(){
+	return "Non-circulating reference book";
+    }
 }
