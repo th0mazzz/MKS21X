74397a64e65f7fea5a7dd44200d93b818fe52d99
Thomas <tzhao2@stuy.edu>
2017-11-28 19:14:48 -0500

    Added getters, checkout and return methods

--- 05Library/CirculatingBook.java
+++ 05Library/CirculatingBook.java
@@ -2,9 +2,21 @@ public class CirculatingBook extends LibraryBook{
     private String currentHolder;
     private String dueDate;
 
-    public CirculatingBook(String name, String bookName, String number, String callNum,
-			   String currentHolder, String dueDate){
+    public CirculatingBook(String name, String bookName, String number, String callNum){
 	super(name, bookName, number, callNum);
+	currentHolder = null;
+	dueDate = null;
+    }
+
+    public String getHolder(){return currentHolder;}
+    public String getDate(){return dueDate;}
+
+    public void checkout(String patron, String due){
+	currentHolder = patron;
+	dueDate = due;
+    }
+
+    public void returned(){
 	currentHolder = null;
 	dueDate = null;
     }
