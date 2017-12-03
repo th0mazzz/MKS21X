1cf1980fa97ffe73268a70792c21f6f7cbfc85d1
Thomas <tzhao2@stuy.edu>
2017-12-02 23:52:20 -0500

    Completed getCode(), wrote tests for methods so far

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,16 +1,49 @@
-public class Barcode implements Comparable<Barcode>{
+public class Barcode {
 
     private String zipcode;
+    private String[] key;
 
     public Barcode(String zip){
 	zipcode = zip;
+	key = new String[10];
+	key[0] = "||:::";
+	key[1] = ":::||";
+	key[2] = "::|:|";
+	key[3] = "::||:";
+	key[4] = ":|::|";
+	key[5] = ":|:|:";
+	key[6] = ":||::";
+	key[7] = "|:::|";
+	key[8] = "|::|:";
+	key[9] = "|:|::";
     }
 
     public String getZip(){
 	return zipcode;
     }
 
-    public static void main(String[] args){
+    public String getCode(){
+	String returnCode = "";
+	int sumOfNumbers = 0; //will use after for loop
 
+	returnCode = returnCode + '|'; //guard rail
+	for(int digit = 0; digit < 5; digit++){
+	    int number = zipcode.charAt(digit) - '0'; //gives the number using ASCII
+	    returnCode = returnCode + key[number];
+	    sumOfNumbers = sumOfNumbers + number;
+	}
+	
+	int check = sumOfNumbers % 10;
+	returnCode = returnCode + key[check];
+
+	returnCode = returnCode + "|";
+
+	return returnCode;
+    }
+
+    public static void main(String[] args){
+	Barcode test1 = new Barcode("01234");
+	System.out.println(test1.getZip()); // prints 01234
+	System.out.println(test1.getCode()); //prints |||::::::||::|:|::||::|::|||:::|
     }
 }
