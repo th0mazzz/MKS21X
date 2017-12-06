d063d56588102f02b85864d40b4722001d35fdfa
Thomas <tzhao2@stuy.edu>
2017-12-05 20:56:20 -0500

    Created basic outline for this assignment

---
+++ 08Sorts/Sorts.java
@@ -0,0 +1,14 @@
+public class Sorts{
+    public static String name(){
+	return "10.Zhao.Thomas";
+    }
+
+    /* Selection sort of an int array. */
+    public static void selectionSort(int[] data){
+	int counter = 0;
+	int min = 
+    }
+
+    public static void main(String[] args){
+    }
+}
