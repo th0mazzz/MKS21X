154c948108f628cf64098de6d98ba7b401bc622f
Thomas <tzhao2@stuy.edu>
2017-12-06 23:26:43 -0500

    Implemented selectionSort() and insertionSort() with tests

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Sorts{
     public static String name(){
 	return "09.Zhao.Thomas";
@@ -5,10 +6,64 @@ public class Sorts{
 
     /* Selection sort of an int array. */
     public static void selectionSort(int[] data){
-	int counter = 0;
-	int min = 
+	int min, innerIndex;
+	int minIndex = -1;
+	for(int outerIndex = 0; outerIndex < data.length; outerIndex++){
+	    min = data[outerIndex];
+	    innerIndex = outerIndex;
+	    for(; innerIndex < data.length; innerIndex++){
+		if(min >= data[innerIndex]){
+		    min = data[innerIndex];
+		    minIndex = innerIndex;
+		}
+	    }
+	    int temp = data[minIndex];
+	    data[minIndex] = data[outerIndex];
+	    data[outerIndex] = temp;
+	}
     }
 
+    /* Insertion sort of an int array */
+    public static void insertionSort(int[] data){
+	//loops through each element of the array starting at index 1
+	for(int outerIndex = 1; outerIndex < data.length; outerIndex++){
+	    //compares elememt above to already sorted part of array
+	    for(int innerIndex = 0; innerIndex < outerIndex; innerIndex++){
+		if(data[outerIndex] < data[innerIndex]){ //if the element is less
+		    int insertValue = data[outerIndex];  //than an element in the
+		    int indexMoving = outerIndex;        //the sorted portion of the array,
+		    while(indexMoving != innerIndex){    //then move rest over and insert
+			data[indexMoving] = data[indexMoving - 1];
+			indexMoving--;
+		    }
+		    data[innerIndex] = insertValue;
+		}
+		//if element is not greater than any element in sorted portion of array,
+		//then it is fine where it is
+	    }
+	}
+		    
+    }
+    
     public static void main(String[] args){
+	int[] test = new int[10];
+	for(int index = 0; index < test.length; index++){
+	    test[index] = (int)(Math.random()*100);
+	}
+
+	System.out.println(Arrays.toString(test));
+	selectionSort(test);
+	System.out.println('\n' + "Selection Sort:" + '\n' + Arrays.toString(test));
+
+	System.out.println("--------------------");
+	
+	int[] test2 = new int[10];
+	for(int index = 0; index < test2.length; index++){
+	    test2[index] = (int)(Math.random()*100);
+	}
+
+	System.out.println(Arrays.toString(test2));
+	insertionSort(test2);
+	System.out.println('\n' + "Insertion Sort:" + '\n' + Arrays.toString(test2));
     }
 }
